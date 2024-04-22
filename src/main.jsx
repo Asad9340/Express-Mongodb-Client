42b756b1aeea94c4c572d6d67c9739523c7d8831
Asad9340 <asadulimran1999@gmail.com>
2024-04-22 13:05:31 +0600

    added login form

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Register from './Register';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<Register/>
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
