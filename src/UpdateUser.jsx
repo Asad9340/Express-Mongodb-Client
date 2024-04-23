2c201e2820f12337908d22256af8c58eda7423c1
Asad9340 <asadulimran1999@gmail.com>
2024-04-23 10:05:44 +0600

    added update page

--- src/UpdateUser.jsx
+++ src/UpdateUser.jsx
@@ -1,17 +1,46 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from 'react-router-dom';
 
 function UpdateUser() {
   const user = useLoaderData();
-  console.log(user)
+  console.log(user);
   return (
     <div className="w-screen h-screen">
-      <div className="flex flex-col justify-center items-center h-full">
-        <h3 className="text-3xl font-semibold">{user.name}</h3>
-        <p className="text-2xl font-medium">{user.email}</p>
-        <p>{user._id}</p>
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-4xl font-semibold">Update User</h2>
+        <p>ID : {user._id}</p>
+      </div>
+      <div className=" flex justify-center items-center mt-6">
+        <form className="bg-white w-96">
+          <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+            <input
+              className="pl-2 outline-none border-none"
+              type="text"
+              name="name"
+              defaultValue={user.name}
+              id=""
+              placeholder="Full name"
+            />
+          </div>
+          <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+            <input
+              className="pl-2 outline-none border-none"
+              type="email"
+              name="email"
+              defaultValue={user.email}
+              id=""
+              placeholder="Email Address"
+            />
+          </div>
+          <button
+            type="submit"
+            className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
+          >
+            Login
+          </button>
+        </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default UpdateUser
+export default UpdateUser;
